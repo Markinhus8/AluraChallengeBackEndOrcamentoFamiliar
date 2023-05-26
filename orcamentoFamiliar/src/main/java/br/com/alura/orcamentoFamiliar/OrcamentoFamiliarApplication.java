1545011c58aa4a68919c5c09ab757077cfbcd1c3
Marcos Vizine <82070442+Markinhus8@users.noreply.github.com>
2023-05-26 11:52:02 -0300

    Add projeto spring com maven via upload

---
+++ orcamentoFamiliar/src/main/java/br/com/alura/orcamentoFamiliar/OrcamentoFamiliarApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.orcamentoFamiliar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrcamentoFamiliarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrcamentoFamiliarApplication.class, args);
+	}
+
+}
---
+++ orcamentoFamiliar/src/test/java/br/com/alura/orcamentoFamiliar/OrcamentoFamiliarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.orcamentoFamiliar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrcamentoFamiliarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
