0b8ae1e8e1f0b3d712dd5141db762adf97c75be6
mvizi <mvizi@Markinhus>
2023-06-15 09:43:07 -0300

    classes que interceptam as exceptions e personalizam o json mostrando o erro para o cliente.

---
+++ orcamentoFamiliar/src/main/java/br/com/alura/orcamentoFamiliar/controller/config/validacao/ErroDeFormularioVO.java
@@ -0,0 +1,23 @@
+package br.com.alura.orcamentoFamiliar.controller.config.validacao;
+
+public class ErroDeFormularioVO {
+	
+	private String campo;
+	private String erro;
+	
+	public ErroDeFormularioVO(String campo, String erro) {
+		this.campo = campo;
+		this.erro = erro;
+	}
+
+	public String getCampo() {
+		return campo;
+	}
+
+	public String getErro() {
+		return erro;
+	}
+	
+	
+
+}
