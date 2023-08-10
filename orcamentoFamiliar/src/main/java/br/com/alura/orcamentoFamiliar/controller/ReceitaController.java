3931c6ba95ec258be125dad97668a42007ed856a
mvizi <mvizi@Markinhus>
2023-08-09 22:08:23 -0300

    Endpoint excluir receita concluído e endpoint atualizar receita em andamento.

--- orcamentoFamiliar/src/main/java/br/com/alura/orcamentoFamiliar/controller/ReceitaController.java
+++ orcamentoFamiliar/src/main/java/br/com/alura/orcamentoFamiliar/controller/ReceitaController.java
@@ -5,16 +5,20 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import br.com.alura.orcamentoFamiliar.controller.form.AtualizacaoReceitaForm;
 import br.com.alura.orcamentoFamiliar.controller.form.ReceitaForm;
 import br.com.alura.orcamentoFamiliar.controller.vo.ReceitaVO;
 import br.com.alura.orcamentoFamiliar.modelo.Receita;
@@ -46,29 +50,49 @@ public class ReceitaController {
 
 		return ResponseEntity.ok().body(new ReceitaVO(receita.get()));
 	}
-	
+
 	@PostMapping
 	@Transactional
 	public ResponseEntity<?> cadastrarReceita(@Valid @RequestBody ReceitaForm form, 
 			UriComponentsBuilder uriBuilder) {
-		
+
 		if (form.validarMesmaDescricaoDentroDoMesmoMes(receitaRepository)) {
 			return ResponseEntity
 					.badRequest()
 					.body("Receita duplicada");
 		}
-		
+
 		Receita receita = form.converterParaEntidadeReceita();
 		Receita itemSalvo = receitaRepository.save(receita);
-		
+
 		URI uri = uriBuilder
 				.path("/receitas/{id}")
 				.buildAndExpand(itemSalvo.getId())
 				.toUri();
-		
+
 		return ResponseEntity
 				.created(uri)
 				.body(new ReceitaVO(itemSalvo));
+	}
+
+	@PutMapping("/{id}")
+	@Transactional
+	public ResponseEntity<ReceitaVO> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoReceitaForm form, UriComponentsBuilder uriBuilder) {
+		Optional<Receita> optional = receitaRepository.findById(id);
+		if (optional.isPresent()) {
+			Receita receita = form.atualizar(id, receitaRepository);
+			return ResponseEntity.ok(new ReceitaVO(receita));
+
+		}
+
+		return ResponseEntity.notFound().build();
+	}
+
+	@DeleteMapping("/{id}")
+	@Transactional
+	public ResponseEntity remover(@PathVariable Long id) {
+		receitaRepository.deleteById(id);
+		return ResponseEntity.ok().build();
 	}
 
 }
