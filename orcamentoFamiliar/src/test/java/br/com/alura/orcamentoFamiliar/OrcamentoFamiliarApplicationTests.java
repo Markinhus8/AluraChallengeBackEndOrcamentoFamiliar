9440b79fa958b12e060ff117dd241a2e95396234
mvizi <mvizi@Markinhus>
2023-12-22 15:25:47 -0300

    Inclusão do Jacoco na aplicação para cobertura de testes.

--- orcamentoFamiliar/src/test/java/br/com/alura/orcamentoFamiliar/OrcamentoFamiliarApplicationTests.java
+++ orcamentoFamiliar/src/test/java/br/com/alura/orcamentoFamiliar/OrcamentoFamiliarApplicationTests.java
@@ -3,11 +3,25 @@ package br.com.alura.orcamentoFamiliar;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import br.com.alura.orcamentoFamiliar.modelo.Despesa;
+import junit.framework.Assert;
+
 @SpringBootTest
 class OrcamentoFamiliarApplicationTests {
 
 	@Test
 	void contextLoads() {
+		
+	}
+	
+	@Test
+	public void testJacoco() {
+		
+		//Despesa despesa = new Despesa();
+		
+		//Assert.assertEquals("teste", despesa.getDescricao());
+		
+	
 	}
 
 }
